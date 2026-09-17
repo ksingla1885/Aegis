d0e3b2b972452b252128585edf171610fb34af3f
ketan <ketansingla7988@gmail.com>
2026-09-17 21:51:55 +0530

    Deployment ready

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
   // Ensure hot module replacement doesn't break on cache mismatches
   headers: async () => {
